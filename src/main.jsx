bd52e5e8af468c4569447858f6db6e6eaf74cc59
Shruti Chavda <schavda684@rku.ac.in>
2024-12-24 17:38:50 +0530

    Component-Demo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import Component1 from './test/Component1'
+// import InputTest from './test/Component3'
+import Timetable from './test/timetable';
+
 // import './index.css'
 // import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    Hello World!!
+    {/* <Component1/> */}
+    {/* <InputTest/> */}
+    <Timetable/>
   </StrictMode>,
-)
+);
---
+++ src/test/timetable.jsx
@@ -0,0 +1,92 @@
+import * as React from "react";
+
+class Timetable extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Timetable of 6CEA</h2>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>Time</th>
+                            <th>Monday</th>
+                            <th>Tuesday</th>
+                            <th>Wednesday</th>
+                            <th>Thursday</th>
+                            <th>Friday</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>8:00 to 8:55</td>
+                            <td rowSpan={2}>React</td>
+                            <td rowSpan={2}>React</td>
+                            <td rowSpan={2}>AI/ML</td>
+                            <td rowSpan={2}>React</td>
+                            <td rowSpan={2}>PD</td>
+                        </tr>
+                        <tr>
+                            <td>8:55 to 9:45</td>
+                        </tr>
+                        <tr>
+                            <td>Class/Leb Location</td>
+                            <td>LL1</td>
+                            <td>LL5</td>
+                            <td>LL1</td>
+                            <td>LL1</td>
+                            <td>118</td>
+                        </tr>
+                        <tr>
+                            <td>9:45 to 10:00</td>
+                            <td colSpan={5} align="center">TEA BREAK</td>
+                        </tr>
+                        <tr>
+                            <td>10:00 to 10:50</td>
+                            <td rowSpan={2}>.NET</td>
+                            <td rowSpan={2}>.NET</td>
+                            <td rowSpan={2}>TOC</td>
+                            <td rowSpan={2}>AI/ML</td>
+                            <td>AI/ML</td>
+                        </tr>
+                        <tr>
+                            <td>10:50 to 11:40</td>
+                            <td>TOC</td>
+                        </tr>
+                        <tr>
+                            <td>Class/Leb Location</td>
+                            <td>LL1</td>
+                            <td>LL1</td>
+                            <td>201</td>
+                            <td>LL6</td>
+                            <td>117</td>
+                        </tr>
+                        <tr>
+                            <td>11:40 to 12:30</td>
+                            <td colSpan={5} align="center">LUNCH BREAK</td>
+                        </tr>
+                        <tr>
+                            <td>12:30 to 1:20</td>
+                            <td rowSpan={2}>PD</td>
+                            <td rowSpan={2}>PD</td>
+                            <td rowSpan={2}>PD</td>
+                            <td rowSpan={2}>.NET</td>
+                            <td rowSpan={2}>.NET</td>
+                        </tr>
+                        <tr>
+                            <td>1:20 to 2:10</td>
+                        </tr>
+                        <tr>
+                            <td>Class/Leb Location</td>
+                            <td>Library</td>
+                            <td>118</td>
+                            <td>Library</td>
+                            <td>LL3</td>
+                            <td>Library</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+export default Timetable;
